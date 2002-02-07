55bfcbb0b00275446cd9e6b1cf59ea7b1ea89668
TOMITA Masahiro <tommy@tmtm.org>
2002-02-08 01:12:55 +0900

    *** empty log message ***

--- tpops_auth-passwd.rb
+++ tpops_auth-passwd.rb
@@ -1,12 +1,15 @@
-# $Id: tpops_auth-passwd.rb,v 1.1 2002/02/06 17:58:02 tommy Exp $
+# $Id: tpops_auth-passwd.rb,v 1.2 2002/02/07 16:12:55 tommy Exp $
 
 require 'etc'
 
 class TPOPS
   class Auth
 
+    PasswdLockDir = '/var/tmp/tpops' unless defined? PasswdLockDir
+    APOPPasswd = '/etc/tpops-apoppw' unless defined? APOPPasswd
+
     def Auth::apop?()
-      false
+      true
     end
 
     def initialize(user, pass, apop=nil)
@@ -16,16 +19,26 @@ class TPOPS
 	return
       end
       @login, @uid, @maildir = pw.name, pw.uid, pw.dir+'/Maildir/'
-      if pass.crypt(pw.passwd) == pw.passwd then
-	@authorized = true
-	return
-      end
-
-      if pw.passwd == 'x' then
-	require 'shadow'
-	sh = Shadow::Passwd.getspnam @login
-	if sh and pass.crypt(sh.sp_pwdp) == sh.sp_pwdp then
+      if apop then
+	dbm = GDBM::open(APOPPasswd, 0600)
+	p = dbm[@login]
+	dbm.close
+	return unless p
+	if pass == MD5::new(apop+pw).hexdigest then
 	  @authorized = true
+	end
+	return
+      else
+	if pass.crypt(pw.passwd) == pw.passwd then
+	  @authorized = true
+	  return
+	end
+	if pw.passwd == 'x' then
+	  require 'shadow'
+	  sh = Shadow::Passwd.getspnam @login
+	  if sh and pass.crypt(sh.sp_pwdp) == sh.sp_pwdp then
+	    @authorized = true
+	  end
 	end
       end
     end
@@ -35,5 +48,18 @@ class TPOPS
     end
 
     attr_reader :login, :uid, :maildir
+
+    def lock()
+      lockfile = "#{PasswdLockDir}/#{@uid}"
+      Dir::mkdir PasswdLockDir, 0700 unless File::exists? PasswdLockDir
+      File::open(lockfile, File::RDWR|File::CREAT|File::EXCL, 0600).close rescue return false
+      true
+    end
+
+    def unlock()
+      lockfile = "#{PasswdLockDir}/#{@uid}"
+      File::unlink lockfile
+    end
+
   end
 end
