118a0d66a3d9022d402f2acbb658eb884d13a7d3
TOMITA Masahiro <tommy@tmtm.org>
2004-06-11 01:13:49 +0900

    *** empty log message ***

--- tpops_auth-mysql.sql
+++ tpops_auth-mysql.sql
@@ -1,4 +1,4 @@
-# $Id: tpops_auth-mysql.sql,v 1.1 2002/12/03 16:26:34 tommy Exp $
+# $Id: tpops_auth-mysql.sql,v 1.2 2004/06/10 16:13:49 tommy Exp $
 
 create table user (
     uid int not null auto_increment,
@@ -10,13 +10,4 @@ create table user (
     primary key (uid),
     unique (login),
     unique (mail)
-);
-
-create table locks (
-    uid int not null,
-    pid int not null,
-    host char(32) not null,
-    timestamp timestamp not null,
-    unique (uid),
-    index (host)
 );
