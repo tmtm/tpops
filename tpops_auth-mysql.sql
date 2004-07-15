7df250ac78c3fb66502af5f0588ad50d2f35d91b
TOMITA Masahiro <tommy@tmtm.org>
2004-07-15 22:33:51 +0900

    0.6.1

--- tpops_auth-mysql.sql
+++ tpops_auth-mysql.sql
@@ -1,9 +1,9 @@
-# $Id: tpops_auth-mysql.sql,v 1.2 2004/06/10 16:13:49 tommy Exp $
+# $Id: tpops_auth-mysql.sql,v 1.3 2004/07/15 13:33:51 tommy Exp $
 
 create table user (
     uid int not null auto_increment,
     login char(16) not null,
-    passwd char(16) not null,
+    passwd char(40) not null,
     mail char(64) not null,
     maildir char(64) not null,
     quota int not null,
